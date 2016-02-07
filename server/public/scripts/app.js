c6a24213679dcf07ee383b85cee946c54f87c9ec
Alex Minge <aminge37@gmail.com>
2016-02-07 14:47:54 -0600

    Initial commit. Right now clicking on one of the numbers will remove the current person that is displayed, but I can't figure out how to get the person whose number you clicked on to appear.

---
+++ server/public/scripts/app.js
@@ -0,0 +1,64 @@
+var onPerson = 1;
+
+$(document).ready(function(){
+    getData();
+});
+
+function getData(){
+    $.ajax({
+        type: "GET",
+        url:"/data",
+        success: function(data) {
+
+            personArray = data.people;
+
+            for (i = 1; i < (personArray.length + 1); i++) {
+                $('.pagination').append('<li><a href="#">' + i + '</a></li>')
+            }
+
+            displayPerson(personArray[onPerson - 1]);
+
+            $('#leftButton').on('click', function() {
+                removePerson();
+                onPerson--;
+                if (onPerson < 1) {
+                    onPerson += personArray.length;
+                }
+                displayPerson(personArray[onPerson - 1])
+            });
+
+            $('#rightButton').on('click', function() {
+                removePerson();
+                onPerson++;
+                if (onPerson > personArray.length) {
+                    onPerson -= personArray.length;
+                }
+                displayPerson(personArray[onPerson - 1]);
+            });
+
+            $('.pagination').on('click', 'li', function() {
+                removePerson();
+
+            });
+
+
+        },
+        error: function() {
+            console.log('ERROR: Unable to contact the server.');
+        }
+
+    });
+}
+
+function displayPerson(person) {
+    $('#personContainer').append('<div><h2>' + person.name + '</h2><p>' + person.favoriteMovie1 + '</p><p>'
+        + person.favoriteMovie2 + '</p><p>' + person.favoriteSong + '</p></div>');
+    //$('#personContainer').append('<h2>' + person.name + '</h2>');
+    //$('#personContainer').append('<p>' + person.favoriteMovie1 + '</p>');
+    //$('#personContainer').append('<p>' + person.favoriteMovie2 + '</p>');
+    //$('#personContainer').append('<p>' + person.favoriteSong + '</p>');
+}
+
+function removePerson() {
+    $('#personContainer').children().remove();
+}
